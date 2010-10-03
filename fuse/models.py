3907663761e396a74a0361233ca1d5ee83143ce3
Matthew Leach <matt@fusefm.co.uk>
2010-10-03 12:03:16 +0100

    Model revisions.

--- fuse/models.py
+++ fuse/models.py
@@ -21,17 +21,19 @@ class mailingList(models.Model):
 	def __unicode__(self):
 		return self.title
 
-class fuseUser(User):
+class fuseUser(models.Model):
+	idnumber = models.CharField('ID Number', max_length = 7)
 	mailingList = models.ManyToManyField(mailingList)
-	firstName = models.CharField(max_length = 25)
-	lastname = models.CharField(max_length = 25)
-	bio = models.TextField()
-	mobile = models.CharField(max_length = 11)
+	firstName = models.CharField('first name', max_length = 25)
+	lastname = models.CharField('last name', max_length = 25)
+	title = models.CharField('committee title', max_length = 25, blank = True)
+	bio = models.TextField('biography', blank = True)
+	mobile = models.CharField(max_length = 11, blank = True)
+	picture = models.FileField(upload_to='userpics', blank = True)
 	def __unicode__(self):
 		return firstName + " " + lastname
 	def getname(self):
 		return firstName + " " + lastname
-		
 
 class Program(models.Model):
 	name = models.CharField(max_length = 150)
@@ -40,7 +42,7 @@ class Program(models.Model):
 	description = models.TextField()
 	blurb = models.TextField()
 	idea = models.TextField()
-	homepage = models.URLField()
+	homepage = models.URLField(blank = True)
 	dj = models.CharField(max_length = 120)
 	shouldBeOnAir = models.TextField()
 	shouldntBeOnAir = models.TextField()
