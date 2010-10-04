91451e30d1900d04e314b34687096882ea477256
Matthew Leach <matt@fusefm.co.uk>
2010-10-04 15:09:03 +0100

    Model revisions.

--- fuseUsers/models.py
+++ fuseUsers/models.py
@@ -10,29 +10,34 @@ class fuseUser(models.Model):
 	idnumber = models.CharField('ID Number', max_length = 7)
 	mailingList = models.ManyToManyField(mailingList, blank = True)
 	firstName = models.CharField('first name', max_length = 25)
-	lastname = models.CharField('last name', max_length = 25)
+	lastName = models.CharField('last name', max_length = 25)
 	email = models.EmailField()
 	title = models.CharField('Committee Title', max_length = 25, blank = True)
 	bio = models.TextField('biography', blank = True)
 	mobile = models.CharField(max_length = 11, blank = True)
 	picture = models.FileField(upload_to='userpics', blank = True)
 	def __unicode__(self):
-		return firstName + " " + lastname
+		return self.idnumber + ' (' +  self.firstName + " " + self.lastName + ')'
 	def getname(self):
 		return firstName + " " + lastname
+	def save(self):
+		saveUserToLDAP(self)
+		super(fuseUser, self).save()
+	def syncUsers(self):
+		super(fuseUser, self).save()
 
 class Program(models.Model):
-	name = models.CharField(max_length = 150)
-	genre = models.CharField(max_length = 50)
+	name = models.CharField('Show Name', max_length = 150)
+	genre = models.CharField('Genre of show', max_length = 50)
 	presenters = models.ManyToManyField(fuseUser)
-	description = models.TextField()
-	blurb = models.TextField()
-	idea = models.TextField()
-	homepage = models.URLField(blank = True)
-	dj = models.CharField(max_length = 120)
-	shouldBeOnAir = models.TextField()
-	shouldntBeOnAir = models.TextField()
-	datetime = models.CharField(max_length = 100)
+	description = models.TextField('Description Of Your Idea')
+	blurb = models.TextField('Show Blurb')
+	idea = models.TextField('One Original Idea For Your Show')
+	homepage = models.URLField('Show Homepage', blank = True)
+	dj = models.CharField('Favourite Radio DJ', max_length = 120)
+	shouldBeOnAir = models.TextField('Why You Should Be On Air')
+	shouldntBeOnAir = models.TextField(' Why You Shouldnt Be On Air')
+	datetime = models.CharField('Prefered Time', max_length = 100)
 	def __unicode__(self):
 		return self.name
 	
